8ada9b0e9dc7a1e5bfbc42051623abdbdb51efd6
Giorgio Calderone <giorgio.calderone@gmail.com>
2020-03-26 09:52:57 +0100

    Docs updated

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter, Gnuplot
 
 makedocs(sitename="Gnuplot.jl",
          authors = "Giorgio Calderone",
-         format = Documenter.HTML(prettyurls = false),  # uncomment for local use, comment for deployment
+         #format = Documenter.HTML(prettyurls = false),  # uncomment for local use, comment for deployment
          modules=[Gnuplot],
          pages = [
              "Home" => "index.md",
@@ -13,35 +13,3 @@ makedocs(sitename="Gnuplot.jl",
              "Examples" => "examples.md",
              "API" => "api.md"
          ])
-
-#=
-- Make documentation:
-cd <repo>/docs
-julia --color=yes make.jl
-
-
-- Workflow to prepare `gh-pages` branch:
-
-Change to a temporary directory, then:
-git clone --no-checkout https://github.com/gcalderone/Gnuplot.jl.git
-git checkout --orphan gh-pages
-git rm -rf .
-
-Now copy the documentation "build" directory into, e.g., "dev", then
-git add dev/*
-git commit -m 'First commit'
-git push origin gh-pages
-
-
-- Workflow to push changes to `gh-pages` branch:
-Change to a temporary directory, then:
-git clone --single-branch --branch gh-pages  https://github.com/gcalderone/Gnuplot.jl.git
-
-Now copy the documentation "build" directory into, e.g., "dev", then
-git add dev/*
-git commit -m 'Docs updated'
-git push origin gh-pages
-
-- Documentation will be available online at:
-https://gcalderone.github.io/Gnuplot.jl/dev
-=#
