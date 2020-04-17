fa870669215a6fd8cd237c391604ac30ada002ef
Giorgio Calderone <giorgio.calderone@gmail.com>
2020-04-18 00:03:41 +0200

    Updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -170,7 +170,7 @@ else
     c = Meta.parse(gpexec("print c"))
 end
 
-@gp    :dry "set term unknwon" :-  #  avoid errors in CI
+@gp    :dry "set term unknown" :-  #  avoid errors in CI
 @gp :- :dry "f(x) = a * sin(b + c*x); a = 1; b = 1; c = 1;"  :-
 @gp :- :dry "a = $a; b = $b; c = $c"                         :-
 @gp :- :dry "set multiplot layout 2,1" ylab="Data and model" :-
